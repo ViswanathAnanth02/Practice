df8f4912ea3edb319bc31f820b82b69fbe0ef0ea
ViswanathAnanth02 <viswanathananth01@gmail.com>
2022-11-11 12:50:32 +0530

    Changed to Subtract

--- sum.java
+++ sum.java
@@ -2,8 +2,8 @@ public class sum {
     public static void main(String[] args) {
        int a=10;
        int b=50;
-       int add=a+b;
+       int add=a-b;
        System.out.print(add);
        
        }
-   }
+    }
