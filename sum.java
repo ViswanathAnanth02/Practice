4ca138c8719d02a29464ee7c0f45b4b01aff32bf
ViswanathAnanth02 <117915084+ViswanathAnanth02@users.noreply.github.com>
2022-11-11 12:51:11 +0530

    Changed to Multiply

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum {
     public static void main(String[] args) {
        int a=10;
        int b=50;
-       int add=a+b;
+       int add=a*b;
        System.out.print(add);
        
        }
